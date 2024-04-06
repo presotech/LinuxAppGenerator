4a2d3ad8b61768f5150cc1eb8440d7dcc38b44a8
Preso <119664913+presotech@users.noreply.github.com>
2024-04-06 00:10:18 -0600

    Create gen_app_file.py

---
+++ gen_app_file.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+
+name = input("Enter app name: ")
+desc = input("Enter app description: ")
+icon = input("Drag and drop icon path: ").split("'")[1]
+path = input("Drag and drop executable path: ")
+
+tags = []
+tag = ""
+
+while True:
+	tag = input("Enter tag (enter quit when done): ")
+	
+	if tag != "quit":
+		tags.append(tag.strip())
+	else:
+		break
+
+tags = ";".join(tags)
+
+file = f"""
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name={name}
+Comment={desc}
+Icon={icon}
+Exec={path}
+Terminal=false
+Categories={tags}"""
+
+with open(f"{name}.desktop", "w") as desktop_file:
+	desktop_file.write(file)
+
+os.system(f"mv {name}.desktop ~/.local/share/applications")
+
+print("Entry complete!")
